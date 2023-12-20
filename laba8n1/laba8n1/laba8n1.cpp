07ecdd0a2bb0cd7cd39b34cae99c02f086edfae0
Kr0sHkA <pisleginkrut@gmail.com>
2023-12-20 21:00:25 +0700

    laba8
    laba8

---
+++ laba8n1/laba8n1/laba8n1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+/*
+ * Л/р 8 Строки
+ * Пислегин Евгений Константинович ИВТ - 32
+ * Дана строка. Преобразовать в ней
+ * все прописные латинские буквы в строчные.
+*/
+
+using namespace std;
+
+void change(string& str)
+{
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] = tolower(str[i]);
+		}
+	}
+}
+
+int main()
+{
+	string str;
+	getline(cin, str);
+	change(str);
+	cout << str;
+}
